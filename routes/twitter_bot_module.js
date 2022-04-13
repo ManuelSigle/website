e1c40418cc9f41e1adc3ef666dd52cc59e9dcdc8
Manuel Sigle <manuel.sigle@gmx.de>
2022-04-13 21:48:39 +0200

    changes

--- routes/twitter_bot_module.js
+++ routes/twitter_bot_module.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const router = express.Router();
+const path = require('path');
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "website_pages", "twitter_bot", "twitter_bot.html"));
+});
+
+
+module.exports = router;
